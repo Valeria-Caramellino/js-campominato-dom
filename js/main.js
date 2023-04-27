9b8d2b714216106a49cb7c65920ca07f3207cb23
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-27 17:21:24 +0200

    cambio colore se mi trovo una bomba

--- js/main.js
+++ js/main.js
@@ -4,6 +4,18 @@ const contentitore = document.getElementById("container");
 //creo collegamento con il pulsante
 const buttonPlay = document.getElementById("play");
 
+const numeroDiCelleTotali = document.getElementById("livelloDifficolta").value;
+
+// genera l'array di bombe
+const bombArray = [];
+ while (bombArray.length < 16) {
+     let bombNum = Math.floor(Math.random() * numeroDiCelleTotali) + 1;
+     if (bombArray.includes(bombNum) == false) {
+     bombArray.push(bombNum);
+     }
+ }
+ console.log(bombArray);
+
 //genero evento per il pulsante richiamando la funzione
 buttonPlay.addEventListener("click", function(){
 
@@ -16,20 +28,35 @@ var inizioGame = false;
 //creo una funzione per generare le caselle con all'interno il numero
 function GeneraCaselleNumerate (numeroDaGenerare){
     if(inizioGame === false){
-        let numeroDiCelleTotali = document.getElementById("livelloDifficolta").value;
+        
         let numeroDiCellePerLato = Math.ceil( Math.sqrt(numeroDiCelleTotali) );
         let dimensione = `calc(100% / ${numeroDiCellePerLato})`;
+
         for (let i = 1; i <= numeroDaGenerare; i++) {
             
-            let box= createElement("div" ,"miniBox100" , i );
+
+            const box= createElement("div" ,"miniBox100" , i );
             box.style.width = dimensione;
             box.style.height = dimensione;
-            
             contentitore.append(box);
 
-            box.addEventListener("click", function() {
-            console.log(i);
-            this.classList.toggle("bgNuovo");
+
+            box.addEventListener("click", function() {    
+            //dichiaro la bomba
+               let Bomba= false;
+                for(let c=0; c<= bombArray.length; c++){
+                    const bombs = bombArray[c];
+                    if(bombs == i){
+                        Bomba=true;
+                    }
+                }
+                if(Bomba){
+                    this.classList.toggle("bgBomba");
+                    console.log("i numeri sono uguali",i);
+                }else{
+                    this.classList.toggle("bgNuovo");
+                    console.log("i numeri sono diversi", i);
+                }
             });
         } 
         inizioGame=true;
@@ -50,3 +77,4 @@ function createElement(tagHtml, classe, contenuto) {
 
     return newBox;
 }
+
